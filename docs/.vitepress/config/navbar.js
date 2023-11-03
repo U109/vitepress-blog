9a6786571e30ade627129f9c3a2b809df2e1fe75
zhang-zhongzhen <1098598203@qq.com>
2023-11-03 15:14:10 +0800

    修改目录结构

--- docs/.vitepress/config/navbar.js
+++ docs/.vitepress/config/navbar.js
@@ -1,33 +1,66 @@
 export default [
-  {
-    text: "数据库",
-    items: [
-      {
-        text: "MySQL",
-        link: "/数据库/MySQL/字符集和比较规则/字符集和比较规则简介",
-      },
-    ],
-  },
-  {
-    text: "框架配置",
-    items: [
-      {
-        text: "VitePress",
-        link: "/框架配置/VitePress/图片缩放功能",
-      },
-    ],
-  },
-  {
-    text: "书籍下载",
-    items: [
-      {
-        text: "Java",
-        link: "/书籍下载/Java/Java",
-      },
-      {
-        text: "网络",
-        link: "/书籍下载/网络/网络",
-      },
-    ],
-  },
+    {
+        text: "首页",
+        link: "/",
+    },
+
+    {
+        text: "数据库",
+        items: [
+            {
+                text: "书籍鉴赏",
+                items: [
+                    {
+                        text: "MySQL是怎样运行的",
+                        link: "/数据库/书籍鉴赏/MySQL是怎样运行的/字符集和比较规则/字符集和比较规则简介",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        text: "开发秘籍",
+        items: [
+            {
+                text: "招式下三滥",
+                items: [
+                    {
+                        text: "踢裆、插眼、搞偷袭",
+                        link: "/开发秘籍/招式下三滥/降低代码可读性的12个技巧",
+                    },
+                ],
+            },
+            {
+                text: "葵花宝典",
+                items: [
+                    {
+                        text: "测试",
+                        link: "/开发秘籍/葵花宝典/测试",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        text: "框架配置",
+        items: [
+            {
+                text: "VitePress",
+                link: "/框架配置/VitePress/图片缩放功能",
+            },
+        ],
+    },
+    {
+        text: "书籍下载",
+        items: [
+            {
+                text: "Java",
+                link: "/书籍下载/Java/Java",
+            },
+            {
+                text: "网络",
+                link: "/书籍下载/网络/网络",
+            },
+        ],
+    },
 ];
